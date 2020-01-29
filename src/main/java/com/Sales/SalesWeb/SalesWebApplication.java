8ef9e584a6364585f244b7730958648930261eaa
Anton <w.zzzz@mail.ru>
2020-01-29 21:25:43 +0300

    [вне задач] - java version

--- src/main/java/com/Sales/SalesWeb/SalesWebApplication.java
+++ src/main/java/com/Sales/SalesWeb/SalesWebApplication.java
@@ -3,7 +3,6 @@ package com.Sales.SalesWeb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//exclude = {DataSourceAutoConfiguration.class }
 @SpringBootApplication()
 public class SalesWebApplication {
 
